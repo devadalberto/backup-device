80e97a63afba3ea5199ac5d5ebe2ff06b45ea9b5
Jose Adalberto Valdes <devadalberto@gmail.com>
2025-06-12 12:25:33 -0700

    Add gallery view and template

--- backend/media_lake/views.py
+++ backend/media_lake/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import MediaFile
+
+
+def gallery(request):
+    """Display thumbnails and metadata for all ``MediaFile`` instances."""
+
+    media_files = MediaFile.objects.all()
+    return render(request, "media_lake/gallery.html", {"media_files": media_files})
